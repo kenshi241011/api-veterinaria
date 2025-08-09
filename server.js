fee0566df3417a35d4dd3576d04763753db6dc3c
kensi24101114 <jorgefarfan2411@gmail.com>
2025-08-09 18:31:59 -0500

    Versión limpia del proyecto

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { Pool } = require('pg'); // Asegúrate de que usa 'pg'
+
+const app = express();
+// Render te dará un puerto, así que es mejor usar esta línea
+const port = process.env.PORT || 3000; 
+
+// ¡CONFIGURACIÓN NUEVA!
+// Reemplaza los datos con los que copiaste de Supabase
+const pool = new Pool({
+  user: 'postgres',
+  host: 'db.lgbwtsxapodtzlpfmhe.supabase.co',
+  database: 'postgres',
+  password: 'Video123', // La que guardaste al crear el proyecto
+  port: 5432,
+});
+// La ruta de la API
+app.get('/api/mascotas', async (req, res) => {
+    try {
+        // En PostgreSQL, es común usar comillas dobles para los nombres de tablas
+        const result = await pool.query('SELECT * FROM "Mascotas"'); 
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err.message);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`API de la veterinaria escuchando en el puerto ${port}`);
+});
